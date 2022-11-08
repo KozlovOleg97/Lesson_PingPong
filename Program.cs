3a2367c0c6ecea5eb3f34bd37b13cb5e58a14fde
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-08 21:22:23 +0300

    Work it!!!

--- Program.cs
+++ Program.cs
@@ -2,12 +2,17 @@
 using PingPong;
 
 void DisplayMessagePong(string message) => Console.WriteLine(message);
-void DisplayMessagePon(string message) => Console.WriteLine(message);
+void DisplayMessagePing(string message) => Console.WriteLine(message);
 
 Ping ping = new Ping();
 Pong pong = new Pong();
 
 ping.onReadPing += DisplayMessagePong;
+pong.onReadPong += DisplayMessagePing;
+ping.Pong();
+pong.Ping();
+ping.Pong();
+pong.Ping();
 ping.Pong();
 pong.Ping();
 Console.WriteLine();
