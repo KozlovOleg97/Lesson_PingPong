0dd55941b648da405c2f199fb8648c17ca405709
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-08 21:19:12 +0300

    Add project files.

---
+++ Ping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+
+    public delegate void PingHandler(string message);
+    internal class Ping
+    {
+        public event PingHandler? onReadPing;
+        public void Pong()
+        {
+            onReadPing?.Invoke($"Ping received Pong");
+        }
+    }
+
+
+    internal class Pong 
+    {
+        public event PingHandler? onReadPong;
+        public void Ping()
+        {
+            onReadPong?.Invoke($"Pong received Ping");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using PingPong;
+
+void DisplayMessagePong(string message) => Console.WriteLine(message);
+void DisplayMessagePon(string message) => Console.WriteLine(message);
+
+Ping ping = new Ping();
+Pong pong = new Pong();
+
+ping.onReadPing += DisplayMessagePong;
+ping.Pong();
+pong.Ping();
+Console.WriteLine();
+
+
